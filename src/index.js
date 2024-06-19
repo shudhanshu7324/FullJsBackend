2c133ca49bca1a82b8cbeeeff3910186cb050173
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-06-19 23:13:30 +0530

    running the backend server in best way setting up server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 
 import 'dotenv/config'
 import connectDb from './db/index.js'
+import { app } from './app.js';
 
 // anytime using database there is a chance that we get some errors so always use try or catch or promise (resolve,reject)
 // always use async await for database connection because it is in another continents takes time to fetch data.
 
-connectDb();
+// when ever async task is done it returns a promise so here we can put .then and .catch to handle the promise
+connectDb().
+    then(()=>{
+        app.listen(process.env.PORT,()=>{
+            console.log("Server is running on Port: ",process.env.PORT);
+        });
+    }).
+    catch((err) => {
+        console.log("MongoDb connection failed ", err);
+    })
 
 
 // mongodb connection using iife
