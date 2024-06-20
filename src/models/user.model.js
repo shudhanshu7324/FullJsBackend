deb7441ac424a2040614fc95d5e4429542461a4d
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-06-20 16:33:00 +0530

    access token and refresh token function

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,6 +62,30 @@ userSchema.methods.isPasswordCorrect = async function(password){
 }
 
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+)
+}
 
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+)
+}
 
 export const User = mongoose.model('User',userSchema);
