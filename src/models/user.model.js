9d75d5374fb35687ed17f16491c502d11e56c6c1
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-06-20 16:15:34 +0530

    hashing password and comparing password logic

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import { Timestamp } from "mongodb";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
         username: {
@@ -45,5 +47,21 @@ const userSchema = new mongoose.Schema({
         }
 
 },{timestamps: true})
+
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+
+
 
 export const User = mongoose.model('User',userSchema);
