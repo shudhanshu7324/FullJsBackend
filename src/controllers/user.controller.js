d7b52bb265dd2a9b82fe8838717088902bcbd56e
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-06-22 11:17:12 +0530

    user register completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,7 @@ const registerUser = asyncHandler(async (req,res) => {
         throw new ApiError(400,"All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username,email}]
     })
 
@@ -38,7 +38,12 @@ const registerUser = asyncHandler(async (req,res) => {
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
 
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"avatar image is required");
@@ -61,7 +66,7 @@ const registerUser = asyncHandler(async (req,res) => {
         username: username.toLowerCase()
     })
 
-    const createdUser = await User.findById(user._id)/select(
+    const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,6 +17,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
         // file have been uploaded successfully
         console.log("file is uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
